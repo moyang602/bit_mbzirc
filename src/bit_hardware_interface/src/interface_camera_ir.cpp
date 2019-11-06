48a47ad336c5456988fd9a9b0ff422bcea5cc450
moyang <moyang602@163.com>
2019-11-06 16:44:34 +0800

    添加部分视觉程序

---
+++ src/bit_hardware_interface/src/interface_camera_ir.cpp
@@ -0,0 +1,11 @@
+#include <ros/ros.h> 
+#include <std_msgs/String.h> 
+
+int main (int argc, char** argv) 
+{ 
+    //初始化节点 
+    ros::init(argc, argv, "interface_camera_ir"); 
+    //声明节点句柄 
+    ros::NodeHandle nh; 
+
+}
