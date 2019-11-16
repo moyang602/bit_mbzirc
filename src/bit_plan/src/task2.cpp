394a312cc93a8d321e0a21e479472cae6890f217
moyang <moyang602@163.com>
2019-11-16 14:20:35 +0800

    修改action服务器与客户端形式

--- src/bit_plan/src/task2.cpp
+++ src/bit_plan/src/task2.cpp
@@ -8,30 +8,89 @@
 
 typedef actionlib::SimpleActionClient<bit_plan::buildingAction> Client;
 
-/*
- *action完成时的回调函数，一次性
- */
-void doneCd(const actionlib::SimpleClientGoalState& state, const bit_plan::buildingResultConstPtr& result)
+class BuildingActionClient
 {
+public:
+    BuildingActionClient(const std::string client_name, bool flag = true):client(client_name, flag) // 使用初始化列表来初始化client
+    {
+        ClientName = client_name;
+    }
+
+    // 客户端开始
+    void Start()
+    {
+        // 等待服务器初始化完成
+        ROS_INFO_STREAM("Waiting for UGV action server: ["<<ClientName<<"] to start!");
+        client.waitForServer();
+        ROS_INFO_STREAM("UGV action server: ["<<ClientName<<"] start!");
+    }
+
+    void sendGoal(bit_plan::buildingGoal goal, double Timeout = 10.0)
+    {
+        // 发送目标至服务器
+        client.sendGoal(goal,
+                        boost::bind(&BuildingActionClient::done_cb, this, _1, _2),
+                        boost::bind(&BuildingActionClient::active_cb, this),
+                        boost::bind(&BuildingActionClient::feedback_cb, this, _1));
+        
+        // 等待完成，超时时间Timeout s
+        if(client.waitForResult(ros::Duration(Timeout)))    // 如果目标完成
+        {
+            ROS_INFO_STREAM("Task: ["<<ClientName<<"] finished within the time");
+        }
+        else
+        {
+            ROS_INFO_STREAM("Task: ["<<ClientName<<"] failed, cancel Goal");
+            client.cancelGoal();
+        }
+        ROS_INFO("Current State: %s\n", client.getState().toString().c_str());
+    }
+
+
+private:
+    Client client;
+    std::string ClientName;      // action名称
     
-    ROS_INFO("Building task finished, State: %d",result->finish_state);
- //   ros::shutdown();
-}
+private:
+    /*
+    *action完成时的回调函数，一次性
+    */
+    void done_cb(const actionlib::SimpleClientGoalState& state, const bit_plan::buildingResultConstPtr& result)
+    {
+        
+        ROS_INFO("Building task finished, State: %d",result->finish_state);
+    //   ros::shutdown();
+    }
 
-/*
- *action启动时的回调函数，一次性
- */
-void activeCd()
-{
-    ROS_INFO("Ugv start building");
-}
+    /*
+    *action启动时的回调函数，一次性
+    */
+    void active_cb()
+    {
+        ROS_INFO("Ugv start building");
+    }
 
-/*
- *action收到反馈时的回调函数
- */
-void feedbackCb(const bit_plan::buildingFeedbackConstPtr& feedback)
+    /*
+    *action收到反馈时的回调函数
+    */
+    void feedback_cb(const bit_plan::buildingFeedbackConstPtr& feedback)
+    {
+        ROS_INFO_STREAM("The UGV state is "<<feedback->task_feedback);
+    }
+};
+
+int ParseBluePrint(bit_task::BrickInfo &brick)
 {
-    ROS_INFO_STREAM("The UGV state is "<<feedback->task_feedback);
+    //
+    ros::Duration(0.5).sleep();       // 临时占位
+    
+    // 砖块信息赋值
+    brick.Sequence = 1;
+    brick.type = 1;
+    brick.x = 1;
+    brick.y = 1;
+
+    return 0;
 }
 
 
@@ -39,45 +98,37 @@ int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "task2_plan");
     
-    Client client("ugv_building", true);       // 创建小车堆墙action客户端
+    // 任务变量初始化
+    bit_plan::buildingGoal ugv_building_goal;   // action 目标
+    std::vector<bit_task::BrickInfo> ugv_brick; // 砖块信息堆栈
+    bit_task::BrickInfo brick;                  // 单个砖块信息
 
-    /* 等待服务端响应 */
-    ROS_INFO("WAITING FOR UGV ACTION SERVER TO START !");
-    client.waitForServer();
-    ROS_INFO("UGV ACTION SERVER START !");
-
-    bit_plan::buildingGoal ugv_building_goal;
-    std::vector<bit_task::BrickInfo> ugv_brick;
     /* 建立UAV 堆墙action客户端 */
+    BuildingActionClient UGVClient("ugv_building", true);    // 创建小车堆墙action客户端
+    UGVClient.Start();
 
-
-    /* 读取建筑蓝图 */
- //   while (condition)
+    /* 读取建筑蓝图 下发指令*/
+ //   while (condition)     // 当蓝图未读取完成时继续
     {
-        bit_task::BrickInfo brick;
-        brick.Sequence = 1;
-        brick.type = 1;
-        brick.x = 1;
-        brick.y = 1;
+        for (size_t i = 0; i < 3; i++) // 读取特定数量的砖块
+        {
+            // 从建筑蓝图中读取建筑信息
+            ParseBluePrint(brick);
+            
+            // 将砖块压入UGV搬运任务堆栈
+            ugv_brick.insert(ugv_brick.begin(),brick);
+        }
 
-        ugv_brick.insert(ugv_brick.begin(),brick);
-        ugv_brick.insert(ugv_brick.begin(),brick);
-        ugv_brick.insert(ugv_brick.begin(),brick);
-
-        /* 循环调用UAV 与 UGV action 服务器 */
-
-        // UGV action 部分
+        /* 调用UAV 与 UGV action 服务器 */
+        // UGV action 部分 设置目标值
         ugv_building_goal.goal_task.header.stamp = ros::Time::now();
         ugv_building_goal.goal_task.header.frame_id = "map";
         ugv_building_goal.goal_task.Num = 3;    // 从蓝图中获取
         ugv_building_goal.goal_task.bricks = ugv_brick;
-        
-        client.sendGoal(ugv_building_goal, &doneCd, &activeCd, &feedbackCb);
+        // 发送 UGV任务指令
+        UGVClient.sendGoal(ugv_building_goal, 100);     // 发送任务指令 等待100s
     }
-    
-    
-
-    ros::spin();
+     ros::spin();
 
     return 0;
 }
--- src/bit_task/src/task2_action.cpp
+++ src/bit_task/src/task2_action.cpp
@@ -16,143 +16,192 @@
 typedef actionlib::SimpleActionClient<bit_motion::pickputAction> Client;
 typedef actionlib::SimpleActionServer<bit_plan::buildingAction> Server;
 
-int ok = 1;
-/*
- *action完成时的回调函数，一次性
- */
-void pickdoneCd(const actionlib::SimpleClientGoalState& state, const bit_motion::pickputResultConstPtr& result)
+class PickPutActionClient
 {
-    ROS_INFO("DONE:%d", result->finish_state);
-    
-    ok = 0;    
-}
-
-/*
- *action启动时的回调函数，一次性
- */
-void pickactiveCd()
-{
-    ROS_INFO("ACTIVE");
-}
-
-/*
- *action收到反馈时的回调函数
- */
-void pickfeedbackCb(const bit_motion::pickputFeedbackConstPtr& feedback)
-{
-    ROS_INFO_STREAM("THE STATE NOM IS: "<<  feedback -> move_rightnow);
-}
-
-/* 创建搬运action目标 */
-bit_motion::pickputGoal pick_goal;          
-/*
- *建筑 action
- */
-void ExecuteBuildingPlan(const bit_plan::buildingGoalConstPtr& goal, Server* as)
-{
-    bit_plan::buildingFeedback feedback;    /* 创建一个feedback对象 */
-
-    ROS_INFO("The goal brick num is: %d", goal->goal_task.Num);
-
-    /* 判断是否有砖堆信息与放置处信息 */
-    if (true)   // 如果有砖堆信息
+public:
+    PickPutActionClient(const std::string client_name, bool flag = true):client(client_name, flag) // 使用初始化列表来初始化client
     {
-        feedback.task_feedback = "The brick and building position exist";
-        as->publishFeedback(feedback);
+        ClientName = client_name;
     }
-    else        // 如果没有砖堆信息
+
+    // 客户端开始
+    void Start()
     {
-        feedback.task_feedback = "Looking for the position of brick and building";
-        as->publishFeedback(feedback);
-        // 找砖和墙位置，
-        // 然后在建筑位置进行global costmap的设置，放置车辆开过砖墙区域  
-
-        ros::Duration(5).sleep();       // 临时占位     换成action
-
-        feedback.task_feedback = "The position of brick and building has been found";
-        as->publishFeedback(feedback);
+        if (!client.isServerConnected())
+        {
+            // 等待服务器初始化完成
+            ROS_INFO_STREAM("Waiting for action server: ["<<ClientName<<"] to start!");
+            client.waitForServer();
+            ROS_INFO_STREAM("Action server: ["<<ClientName<<"] start!");
+        }        
     }
-    
-    static Client pickclient("pickputAction", true);   // 调用取砖动作服务器
 
-    ROS_INFO("Waiting for pickput action server to start!");
-    pickclient.waitForServer();                 /* 等待取砖服务端响应 */
-    ROS_INFO("pickput action server started !");
-
-    // 循环每个预设砖块信息
-    for (size_t count = 0; count < goal->goal_task.Num; count++)
+    void sendGoal(bit_motion::pickputGoal goal, double Timeout = 10.0)
     {
-        // 根据 goal->goal_task.bricks[count].type 移动至相应颜色砖块处
-        ROS_INFO_STREAM("Move to the brick type: "<<goal->goal_task.bricks[count].type);
+        // 发送目标至服务器
+        client.sendGoal(goal,
+                        boost::bind(&PickPutActionClient::done_cb, this, _1, _2),
+                        boost::bind(&PickPutActionClient::active_cb, this),
+                        boost::bind(&PickPutActionClient::feedback_cb, this, _1));
         
-        ros::Duration(3).sleep();       // 临时占位     换成 service
-
-        // 将砖块搬运至车上
-        ok = 1;
-          
-        pick_goal.goal_brick = goal->goal_task.bricks[count];  // 将队列砖块取出发送给取砖程序
-        pick_goal.task = TASK_GET;
-
-        pickclient.sendGoal(pick_goal, &pickdoneCd, &pickactiveCd, &pickfeedbackCb);        /* 发送目标，并且定义回调函数 */
-
-        while(ok){;}  // 等待搬运完成
-
-        ROS_INFO("Picked %ld brick of %d", count+1, goal->goal_task.Num);
+        // 等待完成，超时时间为10s
+        if(client.waitForResult(ros::Duration(Timeout)))    // 如果目标完成
+        {
+            ROS_INFO_STREAM("Task: ["<<ClientName<<"] finished within the time");
+        }
+        else
+        {
+            ROS_INFO_STREAM("Task: ["<<ClientName<<"] failed, cancel Goal");
+            client.cancelGoal();
+        }
+        ROS_INFO("Current State: %s\n", client.getState().toString().c_str());
     }
-    
-    // 移动到观察建筑处，调用检测砖堆状态
-    ROS_INFO("the building state is well");
 
-    ros::Duration(3).sleep();       // 临时占位     换成 service
+private:
+    Client client;
+    std::string ClientName;      // action名称
 
-
-    // 移动至砖块建筑处
-    ROS_INFO("move to the building place");
-
-    ros::Duration(3).sleep();       // 临时占位     换成 service
-
-    // 循环
-    // 移动局部位置，需要使得车辆前进线对准目标位置
-    // 放砖
-    for (size_t count = 0; count < goal->goal_task.Num; count++)
+private:
+    /* action完成时的回调函数，一次性  */
+    void done_cb(const actionlib::SimpleClientGoalState& state, const bit_motion::pickputResultConstPtr& result)
     {
-        // 计算出欲放置砖块相对于目前的位置
-        // 移动
-
-        ros::Duration(3).sleep();       // 临时占位     换成 service
-
-        // 将砖块放置在建筑物处
-        ok = 1;
-
-        pick_goal.goal_brick = goal->goal_task.bricks[count];  // 将队列砖块取出发送给取砖程序
-        pick_goal.task = TASK_BUILD;
-
-        pickclient.sendGoal(pick_goal, &pickdoneCd, &pickactiveCd, &pickfeedbackCb);       /* 发送目标，并且定义回调函数 */
-
-        while(ok){;}  // 等待建筑完成
-
-        ROS_INFO("Put %ld brick of %d", count+1, goal->goal_task.Num);
-
+        ROS_INFO("DONE:%d", result->finish_state);
     }
+    /* action启动时的回调函数，一次性  */
+    void active_cb()
+    {
+        ROS_INFO("ACTIVE");
+    }
+    /* action收到反馈时的回调函数 */
+    void feedback_cb(const bit_motion::pickputFeedbackConstPtr& feedback)
+    {
+        ROS_INFO_STREAM("THE STATE NOM IS: "<<  feedback -> move_rightnow);
+    }
+};
 
-    ROS_INFO("COUNT DONE");
 
-    as->setSucceeded();   /* 发送result */
-}
+class BuildingActionServer
+{
+    public:
+        BuildingActionServer(ros::NodeHandle nh, const std::string server_name, bool flag = false):
+                        server(nh,server_name,boost::bind(&BuildingActionServer::execute_cb, this, _1),flag)
+        {
+            server.registerPreemptCallback(boost::bind(&BuildingActionServer::preempt_cb, this));
+        }
+
+        void Start()
+        {
+            server.start();   
+        }
+
+        /*
+        *建筑 action
+        */
+        void execute_cb(const bit_plan::buildingGoalConstPtr& goal)
+        {
+            bit_plan::buildingFeedback feedback;    /* 创建一个feedback对象 */
+
+            ROS_INFO("The goal brick num is: %d", goal->goal_task.Num);
+
+            static PickPutActionClient Task2Client("pickputAction", true);   // 连接取砖动作服务器
+            Task2Client.Start();
+
+            /* 判断是否有砖堆信息与放置处信息 */
+            if (true)   // 如果有砖堆信息
+            {
+                feedback.task_feedback = "The brick and building position exist";
+                server.publishFeedback(feedback);
+            }
+            else        // 如果没有砖堆信息
+            {
+                feedback.task_feedback = "Looking for the position of brick and building";
+                server.publishFeedback(feedback);
+                // 找砖和墙位置，
+                // 然后在建筑位置进行global costmap的设置，放置车辆开过砖墙区域  
+
+                ros::Duration(5).sleep();       // 临时占位     换成action
+
+                feedback.task_feedback = "The position of brick and building has been found";
+                server.publishFeedback(feedback);
+            }
+
+            // 循环每个预设砖块信息
+            for (size_t count = 0; count < goal->goal_task.Num; count++)
+            {
+                // 根据 goal->goal_task.bricks[count].type 移动至相应颜色砖块处
+                ROS_INFO_STREAM("Move to the brick type: "<<goal->goal_task.bricks[count].type);
+                
+                ros::Duration(3).sleep();       // 临时占位     换成 service
+
+                // 将砖块搬运至车上
+                pick_goal.goal_brick = goal->goal_task.bricks[count];  // 将队列砖块取出发送给取砖程序
+                pick_goal.task = TASK_GET;
+
+                Task2Client.sendGoal(pick_goal, 100);   // 发送目标 timeout 100s
+
+                ROS_INFO("Picked %ld brick of %d", count+1, goal->goal_task.Num);
+            }
+            
+            // 移动到观察建筑处，调用检测砖堆状态
+            ROS_INFO("the building state is well");
+
+            ros::Duration(3).sleep();       // 临时占位     换成 service
 
 
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
+            // 移动至砖块建筑处
+            ROS_INFO("move to the building place");
+
+            ros::Duration(3).sleep();       // 临时占位     换成 service
+
+            // 循环
+            // 移动局部位置，需要使得车辆前进线对准目标位置
+            // 放砖
+            for (size_t count = 0; count < goal->goal_task.Num; count++)
+            {
+                // 计算出欲放置砖块相对于目前的位置
+                // 移动
+
+                ros::Duration(3).sleep();       // 临时占位     换成 service
+
+                // 将砖块放置在建筑物处
+                pick_goal.goal_brick = goal->goal_task.bricks[count];  // 将队列砖块取出发送给取砖程序
+                pick_goal.task = TASK_BUILD;
+
+                Task2Client.sendGoal(pick_goal, 100);   // 发送目标 timeout 100s
+
+                ROS_INFO("Put %ld brick of %d", count+1, goal->goal_task.Num);
+
+            }
+
+            ROS_INFO("COUNT DONE");
+
+            server.setSucceeded();   /* 发送result */
+        }
+
+    private:
+        Server server;
+        /* 创建搬运action目标 */
+        bit_motion::pickputGoal pick_goal;   
+        
+        
+        // 中断回调函数
+        void preempt_cb()
+        {
+            if(server.isActive())
+            {
+                server.setPreempted(); // 强制中断
+            }
+        }
+};
+
+
 int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "task2_action");
-
     ros::NodeHandle nh;
 
-    Server server(nh, "ugv_building", boost::bind(&ExecuteBuildingPlan, _1, &server), false);
-
-    server.start();
+    BuildingActionServer UGVServer(nh, "ugv_building", false);
+    UGVServer.Start();
 
     ros::spin();
 
